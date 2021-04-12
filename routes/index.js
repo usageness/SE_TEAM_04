2a4c5c340ce95179f425cb972a3157a0ea941bd8
DESKTOP-0BSVB30\kde12 <kde12327@gmail.com>
2021-04-12 20:51:52 +0900

    CREATE admin router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+var adminRouter = require('./admin');
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -13,5 +17,9 @@ router.get('/notice', function(req, res, next) {
 router.get('/product', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+
+// const app = express();
+router.use('/admin', adminRouter);
 
 module.exports = router;
