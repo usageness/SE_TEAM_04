cb6a06747809e2439953026d05124a9ca8689dd7
DESKTOP-0BSVB30\kde12 <kde12327@gmail.com>
2021-04-12 21:29:31 +0900

    CREATE login view

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -6,4 +6,23 @@ router.get('/', function(req, res, next) {
   res.render('admin_main', { title: 'AdminMain' });
 });
 
+router.get('/login', function(req, res, next) {
+  res.render('admin_login', { title: 'QuickBoard' });
+});
+
+router.post('/login', function(req, res, next) {
+  // TODO: Login logic
+
+
+  res.redirect('/admin');
+});
+
+router.get('/eventad', function(req, res, next) {
+  res.render('admin_eventad', { title: '' });
+});
+
+router.get('/item', function(req, res, next) {
+  res.render('admin_item', { title: '' });
+});
+
 module.exports = router;
