094849abeca5d08c634e3977fc6f8af357b87779
Gyujin Cho <och1939420@gmail.com>
2021-04-26 23:02:34 +0900

    CREATE models/eventad

---
+++ models/eventad.js
@@ -0,0 +1,53 @@
+const Sequelize = require('sequelize');
+
+class Eventad extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            title: {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+                unique: true,
+            },
+            flag: { // 0 = ad , 1 = event
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+            },
+            content: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            start: { // 시작일
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            end: { // 종료일
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+            url: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            imageurl: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            visible: { // 0 = 볼 수 없음 , 1 = 볼 수 있음
+                type: Sequelize.BOOLEAN,
+                allowNull: false,
+            },
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'Eventad',
+            tableName: 'eventads',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci'
+        });
+    }
+    static associate(db) {}
+};
+
+module.exports = Eventad;
