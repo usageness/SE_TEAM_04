114fb4b499a433833c32c73047a46fc4fc8b8162
YongRae_Kim (Usage) <kyr9389@naver.com>
2021-04-26 22:33:13 +0900

    UPDATE app.js login router add
    Signed-off-by: YongRae_Kim (Usage) <kyr9389@naver.com>

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var loginRouter = require('./routes/login');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/login', loginRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
