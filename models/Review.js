ea5c1d7b6dc777c42869094e98184593e89bd7aa
Gyujin Cho <och1939420@gmail.com>
2021-04-26 23:20:23 +0900

    CREATE models/Review

---
+++ models/Review.js
@@ -0,0 +1,37 @@
+const Sequelize = require('sequelize');
+
+class Review extends Sequelize.Model {
+    static init(sequelize) {
+        return super.init({
+            title: {
+                type: Sequelize.STRING(50),
+                allowNull: false,
+                unique: true,
+            },
+            score: {
+                type: Sequelize.INTEGER.UNSIGNED,
+                allowNull: false,
+            },
+            content: {
+                type: Sequelize.TEXT,
+                allowNull: true,
+            },
+            date: { // 작성일
+                type: Sequelize.DATE,
+                allowNull: false,
+            },
+        }, {
+            sequelize,
+            timestamps: false,
+            underscored: false,
+            modelName: 'Review',
+            tableName: 'reviews',
+            paranoid: false,
+            charset: 'utf8',
+            collate: 'utf8_general_ci'
+        });
+    }
+    static associate(db) {}
+};
+
+module.exports = Review;
