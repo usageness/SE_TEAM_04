067c9e33f1247bd235a5bad65c98bbf754db6101
Gyujin Cho <och1939420@gmail.com>
2021-05-11 16:54:56 +0900

    UPDATE address_manage using addressList

--- controllers/addressController.js
+++ controllers/addressController.js
@@ -2,42 +2,45 @@ const Address = require("../models").Address;
 const User = require("../models").User;
 const PurchaseLog = require("../models").PurchaseLog;
 
-
-
-const getAddress = async (req, res) => {
-
-};
-
+const getAddress = async (req, res) => {};
 
 const postAddress = async (req, res) => {
   const {
-    body:{postcode,roadAddress,detailAddress,extraAddress,addressname}
+    body: { postcode, roadAddress, detailAddress, extraAddress, addressname },
   } = req;
-console.log(req.body);
-  await Address.create({
-    name: addressname,
-    address: roadAddress,
-    detailesAddress: detailAddress,
-    postalCode:postcode,
-    phoneNumber:extraAddress
-   
-  },{
-    include:[{
-      model: User, as:"registered_address",foreignKey: "userId",
-    },{
-      model: PurchaseLog, as: "destination",foreignKey: "addressId",
-    }]
-  });
-  res.render('address_manage', { title: 'Express' });
+  console.log(req.body);
+  await Address.create(
+    {
+      name: addressname,
+      address: roadAddress,
+      detailesAddress: detailAddress,
+      postalCode: postcode,
+      phoneNumber: extraAddress,
+      isChecked: 0,
+    },
+    {
+      include: [
+        {
+          model: User,
+          as: "registered_address",
+          foreignKey: "userId",
+        },
+        {
+          model: PurchaseLog,
+          as: "destination",
+          foreignKey: "addressId",
+        },
+      ],
+    }
+  );
+  let address = await Address.findAll({});
+  res.render("address_manage", { title: "Express", data:{
+    addressList:address
+  } });
 };
 
-const updateAddress = async (req, res) => {
+const updateAddress = async (req, res) => {};
 
-};
+const deleteAddress = async (req, res) => {};
 
-const deleteAddress = async (req, res) => {
-
-};
-
-
-module.exports ={postAddress};
+module.exports = { postAddress };
