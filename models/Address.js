8c22f61d381d8869eba790b1c0510e0be1a4746c
Gyujin Cho <och1939420@gmail.com>
2021-05-11 09:33:10 +0900

    UPDATE models/Address, eventad

--- models/Address.js
+++ models/Address.js
@@ -1,37 +1,47 @@
 module.exports = function (sequelize, DataTypes) {
-    var Address = sequelize.define(
-      'Address',
-      {
-        id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            allowNull: false,
-            unique: true,
-            autoIncrement: true,
-        },
-        address: {
-            type: DataTypes.STRING(50),
-            allowNull: false,
-            unique: true,
-        },
+  var Address = sequelize.define(
+    "Address",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        unique: true,
+        autoIncrement: true,
+      },
+      address: {
+        type: DataTypes.STRING(70),
+        allowNull: false,
+      },
+      detailesAddress: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      postalCode: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+      phoneNumber: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
     },
-      {
-        tableName: 'Address',
-        freezeTableName: false,
-        timestamps: false,
-        underscored: false,
-      }
-    )
-    Address.associate = (models) => {
-      Address.belongsTo(models.User, {
-        as: "registered_address",
-        foreignKey: "userId",
-      });
-      Address.hasMany(models.PurchaseLog, {
-        as: "destination",
-        foreignKey: "addressId",
-      });
+    {
+      tableName: "Address",
+      freezeTableName: false,
+      timestamps: false,
+      underscored: false,
     }
-    return Address;
-  }
-  
+  );
+  Address.associate = (models) => {
+    Address.belongsTo(models.User, {
+      as: "registered_address",
+      foreignKey: "userId",
+    });
+    Address.hasMany(models.PurchaseLog, {
+      as: "destination",
+      foreignKey: "addressId",
+    });
+  };
+  return Address;
+};
