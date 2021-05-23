a180ce77ee70115379cfca6e2c5b54fad66eb662
YongRae_Kim (Usage) <kyr9389@naver.com>
2021-05-23 18:37:13 +0900

    MODIFY email exist check logic add
    Signed-off-by: YongRae_Kim (Usage) <kyr9389@naver.com>

--- routes/signUp.js
+++ routes/signUp.js
@@ -16,33 +16,47 @@ router.post('/', function (req, res, next) {
     let salt = Math.round((new Date().valueOf() * Math.random())) + "";
     let hashPassword = crypto.createHash("sha256").update(inputPassword + salt).digest("hex");
 
-    users.User.findOne({
-        where: {
-            user_id: body.user_id
-        }
-    }).then((existCheck) => {
-        if (existCheck === null) {
-            users.User.create({
-                user_id: body.user_id,
-                password: hashPassword,
-                nickname: body.nickname,
-                permission: 0,
-                create_at: Date.now(),
-                salt: salt
+    let existCheckId =
+        users.User.findOne({
+            where: {
+                user_id: body.user_id
+            }
+        });
+
+    let existCheckEmail =
+        users.User.findOne({
+            where: {
+                email: body.email
+            }
+        });
+
+    existCheckId.then((result) => {
+        if(!result) {
+            existCheckEmail.then((result) => {
+                if(!result) {
+                    users.User.create({
+                        user_id: body.user_id,
+                        password: hashPassword,
+                        nickname: body.nickname,
+                        email: body.email,
+                        permission: 0,
+                        create_at: Date.now(),
+                        salt: salt
+                    }).then(() => {
+                        req.session.user_id = body.user_id;
+                        req.session.nickname = body.nickname;
+                        res.send('<script type="text/javascript">alert("회원가입이 완료되었습니다"); location.href = "/";</script>');
+                    }).catch(() => {
+                        res.send('<script type="text/javascript">alert("오류가 발생했습니다"); location.href = "/signUp";</script>');
+                    });
+                }else{
+                    res.send('<script type="text/javascript">alert("중복된 이메일입니다"); location.href = "/signUp";</script>');
+                }
             })
-                .then(result => {
-                    res.send('<script type="text/javascript">alert("회원가입이 완료되었습니다."); location.href = "/";</script>');
-                })
-                .catch(err => {
-                    console.log(err);
-                })
-        }
-        else
-        {
+        }else{
             res.send('<script type="text/javascript">alert("중복된 아이디입니다"); location.href = "/signUp";</script>');
         }
-    });
-
+    })
 });
 
 module.exports = router;
