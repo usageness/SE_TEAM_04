e628213eccbbea8f96bed91ecd16c64368b9edc6
YongRae_Kim (Usage) <kyr9389@naver.com>
2021-05-23 23:21:34 +0900

    MODIFY search by product name logic add
    Signed-off-by: YongRae_Kim (Usage) <kyr9389@naver.com>

--- app.js
+++ app.js
@@ -41,8 +41,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(session({
-  key:process.env.SESSION_KEY,
-  secret: process.env.SESSION_SECRET,
+  key: 'sid',
+  secret: 'secret',
   resave: false,
   saveUninitialized: true,
   cookie: {
