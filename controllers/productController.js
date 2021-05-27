9a2ce0ee30dc8f5a114404cd69c778cba1a6c677
Gyujin Cho <och1939420@gmail.com>
2021-05-28 01:50:05 +0900

    CREATE product_detail price feature

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,4 +1,6 @@
 const Product = require("../models").Product;
+const Category = require("../models").Category;
+
 
 const getProductDetail = async (req,res) => {
   const {
@@ -6,11 +8,12 @@ const getProductDetail = async (req,res) => {
   } =req;
   let session = req.session;
 
-  let product = await Product.findOne({where:{id:productId}});
-
+  let product = await Product.findOne({where:{id:req.params.productId}});
+  let category =await Category.findOne({where:{id:product.categoryinId}})
   res.render("product_detail", { title: "", session,
 data:{
-product
+product,
+category,
 } });
 };
 module.exports = {getProductDetail};
